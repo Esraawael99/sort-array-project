6c2d03fc13756051d048415795aa490811143bf3
Esraa Wael <wael87985@gmail.com>
2024-12-28 01:35:15 +0200

    Initial commit for Sort Array project

---
+++ sortArray.js
@@ -0,0 +1,20 @@
+// sortArray.js
+
+function bubbleSort(arr) {
+    let len = arr.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap elements
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+// Example usage:
+const unsortedArray = [5, 3, 8, 4, 2];
+console.log("Sorted Array:", bubbleSort(unsortedArray));
